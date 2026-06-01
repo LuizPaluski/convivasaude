c07782c6ac38252c5c2c7b9b8f607aaf2b211cb3
Luiz Paluski <luizgustavodelimapalauski@gmail.com>
2026-06-01 10:01:20 -0300

    fix: chama clarity('consent') na tag (site nao tem banner, consentimento implicito) para destravar gravacao do Clarity

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -39,7 +39,8 @@ gtag('config', '${GA4_ID}');`}
 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-})(window, document, "clarity", "script", "${CLARITY_ID}");`}
+})(window, document, "clarity", "script", "${CLARITY_ID}");
+window.clarity && window.clarity("consent");`}
       </Script>
 
       {/* Meta Pixel */}
